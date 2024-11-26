50bfc8e5c88690a50b8191bb0b03cc60f4b0a1eb
SeongUk18 <adulty22@gmail.com>
2024-11-26 16:09:22 +0900

    Feat: 기본 패키지 생성"

---
+++ src/main/java/com/gathering/gathering_backend/GatheringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gathering.gathering_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gathering/gathering_backend/GatheringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gathering.gathering_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatheringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
