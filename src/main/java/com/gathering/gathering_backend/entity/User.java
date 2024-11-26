90dd2f5e46b58f5bd8c2fe6b4ba011c16d616726
SeongUk18 <adulty22@gmail.com>
2024-11-26 17:38:53 +0900

    Feat: User Entity 추가

---
+++ src/main/java/com/gathering/gathering_backend/entity/User.java
@@ -0,0 +1,46 @@
+package com.gathering.gathering_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userNo;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String userId;
+
+    @Column(nullable = false, length = 100)
+    private String userPw;
+
+    @Column(nullable = false, length = 20)
+    private String userName;
+
+    @Column(columnDefinition = "TEXT")
+    private String userDescription;
+
+    @Column(nullable = false)
+    private int userType;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SearchHistory> searchHistories;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Notification> notifications;
+}
