f4a4d19761bebcf0ae5fd1ef651cab0e609e06b2
SeongUk18 <adulty22@gmail.com>
2024-12-20 20:09:55 +0900

    Feat: JWT 기능 구현

---
+++ src/main/java/com/gathering/gathering_backend/util/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.gathering.gathering_backend.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256); // 비밀 키 생성
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1시간
+
+    // 토큰 생성
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    // 토큰에서 사용자 이름 추출
+    public static String extractUserId(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // JWT 유효성 검증
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false; // 토큰이 유효하지 않음
+        }
+    }
+}
