e8c78c9ffd2247585a116ad2d25bbcf715acdb89
SeongUk18 <adulty22@gmail.com>
2024-12-27 13:36:09 +0900

    Feat: 회원 정보 수정 dto 수정

--- src/main/java/com/gathering/gathering_backend/dto/UserUpdateDTO.java
+++ src/main/java/com/gathering/gathering_backend/dto/UserUpdateDTO.java
@@ -3,10 +3,13 @@ package com.gathering.gathering_backend.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class UserUpdateDTO {
     private String userPw; // 비밀번호 (Optional)
     private String userName; // 닉네임 (Optional)
     private String userDescription; // 자기소개 (Optional)
+    private List<Long> interests; // 관심사 ID 리스트
 }
